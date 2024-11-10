54cdb8dc7d8e7534951d3c9962e74e713847f64d
Diego Zarza Morales <l22210366@tectijuana.edu.mx>
2024-11-09 19:44:28 -0800

    Create programa15.s

---
+++ Programas_10-19/programa15.s
@@ -0,0 +1,86 @@
+/*
+Autor: [Zarza Morales Jose Diego] [2221036]
+Propósito: Implementar búsqueda binaria en un arreglo ordenado en ARM64
+
+Código equivalente en C#:
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = {4, 8, 15, 16, 23, 42}; // Arreglo ordenado
+        int buscar = 16;
+        int inicio = 0;
+        int fin = arr.Length - 1;
+        int posicion = -1;
+        
+        while(inicio <= fin)
+        {
+            int medio = (inicio + fin) / 2;
+            if(arr[medio] == buscar)
+            {
+                posicion = medio;
+                break;
+            }
+            if(arr[medio] < buscar)
+                inicio = medio + 1;
+            else
+                fin = medio - 1;
+        }
+        
+        if(posicion != -1)
+            Console.WriteLine($"El elemento {buscar} se encontró en la posición {posicion}");
+        else
+            Console.WriteLine($"El elemento {buscar} no se encontró en el arreglo");
+    }
+}
+*/
+
+.data
+    array:      .word   4, 8, 15, 16, 23, 42
+    size:       .word   6
+    buscar:     .word   16
+    msg_array:  .string "Arreglo ordenado: "
+    msg_num:    .string "%d "
+    msg_found:  .string "\nEl elemento %d se encontró en la posición %d\n"
+    msg_not:    .string "\nEl elemento %d no se encontró en el arreglo\n"
+
+.text
+.global main
+
+main:
+    stp     x29, x30, [sp, #-16]!
+    mov     x29, sp
+
+    // Imprimir arreglo
+    adr     x0, msg_array
+    bl      printf
+
+    // Mostrar elementos
+    adr     x19, array       // x19 = dirección del array
+    adr     x20, size
+    ldr     w20, [x20]       // w20 = tamaño
+    adr     x21, buscar
+    ldr     w21, [x21]       // w21 = valor a buscar
+    mov     w22, #0          // w22 = índice
+
+print_loop:
+    cmp     w22, w20
+    bge     binary_search
+
+    ldr     w1, [x19, w22, SXTW 2]
+    adr     x0, msg_num
+    bl      printf
+
+    add     w22, w22, #1
+    b       print_loop
+
+binary_search:
+    mov     w22, #0          // inicio = 0
+    sub     w23, w20, #1     // fin = tamaño - 1
+
+search_loop:
+    cmp     w22, w23
+    bgt     not_found
+
+    add     w24, w

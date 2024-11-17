959dc6db81ace31f76792ea6e4d3eaebf7efd7da
Diego Zarza Morales <l22210366@tectijuana.edu.mx>
2024-11-16 21:05:15 -0800

    Update programa46.s

--- programas_40-49/programa46.s
+++ programas_40-49/programa46.s
@@ -1,7 +1,6 @@
 /*
 Autor: [Zarza Morales Jose Diego] [2221036]
 Propósito: Encontrar el prefijo común más largo entre dos cadenas en ARM64
-
 Código equivalente en C#:
 using System;
 class Program
@@ -25,7 +24,6 @@ class Program
     }
 }
 */
-
 .data
     str1:           .string "flower"
     str2:           .string "flow"
@@ -33,56 +31,44 @@ class Program
     msg_str1:       .string "String 1: %s\n"
     msg_str2:       .string "String 2: %s\n"
     msg_prefix:     .string "Prefijo común más largo: %s\n"
-
 .text
 .global main
-
 main:
     stp     x29, x30, [sp, #-16]!
     mov     x29, sp
-
     // Imprimir strings originales
     adr     x0, msg_str1
     adr     x1, str1
     bl      printf
-
     adr     x0, msg_str2
     adr     x1, str2
     bl      printf
-
     // Inicializar variables
     adr     x19, str1          // x19 = dirección str1
     adr     x20, str2          // x20 = dirección str2
     adr     x21, prefix        // x21 = dirección prefix
-    mov     w22, #0            // w22 = índice
-
+    mov     x22, #0            // x22 = índice
 compare_loop:
     // Cargar caracteres
-    ldrb    w23, [x19, w22]    // w23 = str1[i]
-    ldrb    w24, [x20, w22]    // w24 = str2[i]
-
+    ldrb    w23, [x19, x22]    // w23 = str1[i]
+    ldrb    w24, [x20, x22]    // w24 = str2[i]
     // Si alguno es nulo o son diferentes, terminar
     cbz     w23, end_prefix
     cbz     w24, end_prefix
     cmp     w23, w24
     bne     end_prefix
-
     // Guardar carácter en prefix
-    strb    w23, [x21, w22]
-
-    add     w22, w22, #1       // i++
+    strb    w23, [x21, x22]
+    add     x22, x22, #1       // i++
     b       compare_loop
-
 end_prefix:
     // Agregar null terminator
     mov     w23, #0
-    strb    w23, [x21, w22]
-
+    strb    w23, [x21, x22]
     // Imprimir resultado
     adr     x0, msg_prefix
     mov     x1, x21
     bl      printf
-
     mov     w0, #0
     ldp     x29, x30, [sp], #16
     ret
